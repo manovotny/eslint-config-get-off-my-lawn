1864918a60cd606a555f553f80191fa1f6d99d4e
Michael Novotny <manovotny@gmail.com>
2016-10-01 22:03:23 -0500

    Adds unicorn plugin rules.
    Fixes #1

---
+++ src/unicorn.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'unicorn/catch-error-name': [
+        'error',
+        {
+            'name': 'error'
+        }
+    ],
+    'unicorn/explicit-length-check': 'error',
+    'unicorn/no-abusive-eslint-disable': 'error',
+    'unicorn/no-process-exit': 'error',
+    'unicorn/throw-new-error': 'error'
+};
