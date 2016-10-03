38650a164b55b16c222bec841cfa48c2c34a6f9e
Michael Novotny <manovotny@gmail.com>
2016-10-03 16:56:43 -0500

    Adds hook rules for mocha plugin.

--- src/mocha.js
+++ src/mocha.js
@@ -2,9 +2,12 @@ module.exports = {
     'mocha/handle-done-callback': 'error',
     'mocha/no-exclusive-tests': 'error',
     'mocha/no-global-tests': 'error',
+    'mocha/no-hooks-for-single-case': 'error',
     'mocha/no-identical-title': 'error',
     'mocha/no-pending-tests': 'error',
     'mocha/no-return-and-callback': 'error',
+    'mocha/no-sibling-hooks': 'error',
     'mocha/no-skipped-tests': 'error',
+    'mocha/no-top-level-hooks': 'error',
     'mocha/valid-test-description': 'error'
 };
