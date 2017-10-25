dff39e03f20f89e7180e1722cafc57ed5377b28b
Michael Novotny <manovotny@gmail.com>
2017-10-25 16:40:38 -0500

    Updates eslint-plugin-unicorn. (#54)

--- src/unicorn.js
+++ src/unicorn.js
@@ -5,7 +5,15 @@ module.exports = {
             name: 'error'
         }
     ],
+    'unicorn/custom-error-definition': 'error',
+    'unicorn/escape-case': 'error',
     'unicorn/no-abusive-eslint-disable': 'error',
+    'unicorn/no-array-instanceof': 'error',
+    'unicorn/no-hex-escape': 'error',
+    'unicorn/no-new-buffer': 'error',
     'unicorn/no-process-exit': 'error',
+    'unicorn/number-literal-case': 'error',
+    'unicorn/prefer-starts-ends-with': 'error',
+    'unicorn/prefer-type-error': 'error',
     'unicorn/throw-new-error': 'error'
 };
