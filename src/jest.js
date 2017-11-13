1db63c378d00f88ab6519deae597e7f9b7748d41
Michael Novotny <manovotny@gmail.com>
2017-11-13 12:43:36 -0600

    Updates all dependencies to latest. (#68)

--- src/jest.js
+++ src/jest.js
@@ -2,5 +2,6 @@ module.exports = {
     'jest/no-disabled-tests': 'error',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'error',
     'jest/valid-expect': 'error'
 };
