915bef5c0350b061ddd6ee9cb1517acbee2f995e
Michael Novotny <manovotny@gmail.com>
2017-11-14 13:10:03 -0600

    Removes rules that cause false positives or are too explicit for the code we're writing.

--- src/security.js
+++ src/security.js
@@ -4,10 +4,6 @@ module.exports = {
     'security/detect-disable-mustache-escape': 'error',
     'security/detect-eval-with-expression': 'error',
     'security/detect-no-csrf-before-method-override': 'error',
-    'security/detect-non-literal-fs-filename': 'error',
-    'security/detect-non-literal-regexp': 'error',
-    'security/detect-non-literal-require': 'error',
-    'security/detect-object-injection': 'error',
     'security/detect-possible-timing-attacks': 'error',
     'security/detect-pseudoRandomBytes': 'error',
     'security/detect-unsafe-regex': 'error'
