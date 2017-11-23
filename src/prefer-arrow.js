b9d4c1e0bbd91de71e8cbc23cf1e0096e1ede909
Michael Novotny <manovotny@gmail.com>
2017-11-23 16:32:00 -0600

    Adds eslint-plugin-prefer-arrow. (#77)

---
+++ src/prefer-arrow.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'prefer-arrow/prefer-arrow-functions': [
+        'error',
+        {
+            'classPropertiesAllowed': false,
+            'disallowPrototype': true,
+            'singleReturnOnly': false
+        }
+    ]
+};
