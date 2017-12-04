17934761ed92379aa314421f87009eeb11f6ac88
Michael Novotny <manovotny@gmail.com>
2017-12-04 13:51:32 -0600

    Removes duplicate rule for missing requires / imports. (#90)

--- src/node.js
+++ src/node.js
@@ -3,22 +3,6 @@ module.exports = {
     'node/no-deprecated-api': 'error',
     'node/no-extraneous-import': 'error',
     'node/no-extraneous-require': 'error',
-    'node/no-missing-import': [
-        'error',
-        {
-            allowModules: [
-                'electron'
-            ]
-        }
-    ],
-    'node/no-missing-require': [
-        'error',
-        {
-            allowModules: [
-                'electron'
-            ]
-        }
-    ],
     'node/no-unpublished-bin': 'error',
     'node/no-unpublished-import': 'error',
     'node/no-unpublished-require': 'error',
