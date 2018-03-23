29dd85dea0bf62eefaf71a913e81467fe4968ab4
Michael Novotny <manovotny@gmail.com>
2018-03-23 15:32:23 -0500

    Updates eslint-plugin-import.

--- src/import.js
+++ src/import.js
@@ -21,7 +21,9 @@ module.exports = {
     'import/no-mutable-exports': 'error',
     'import/no-named-as-default': 'error',
     'import/no-named-as-default-member': 'error',
+    'import/no-self-import': 'error',
     'import/no-unresolved': 'error',
+    'import/no-useless-path-segments': 'error',
     'import/no-webpack-loader-syntax': 'error',
     'import/order': [
         'error',
