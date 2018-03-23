824db2bc6025a669bc8fe98043b2dbde3a6566aa
Michael Novotny <manovotny@gmail.com>
2018-03-23 16:46:11 -0500

    Updates eslint-plugin-jest.

--- src/jest.js
+++ src/jest.js
@@ -1,7 +1,17 @@
 module.exports = {
+    'jest/consistent-test-it': [
+        'error',
+        {
+            fn: 'test',
+            withinDescribe: 'test'
+        }
+    ],
     'jest/no-disabled-tests': 'error',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
+    'jest/prefer-to-be-null': 'error',
+    'jest/prefer-to-be-undefined': 'error',
     'jest/prefer-to-have-length': 'error',
+    'jest/valid-describe': 'error',
     'jest/valid-expect': 'error'
 };
