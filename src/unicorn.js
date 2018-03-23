29e662ad21aed32eff4ba6932bf3a3b3d4a91f90
Michael Novotny <manovotny@gmail.com>
2018-03-23 15:17:07 -0500

    Updates eslint-plugin-unicorn.

--- src/unicorn.js
+++ src/unicorn.js
@@ -6,6 +6,7 @@ module.exports = {
         }
     ],
     'unicorn/custom-error-definition': 'error',
+    'unicorn/error-message': 'error',
     'unicorn/escape-case': 'error',
     'unicorn/new-for-builtins': 'error',
     'unicorn/no-array-instanceof': 'error',
@@ -13,6 +14,7 @@ module.exports = {
     'unicorn/no-new-buffer': 'error',
     'unicorn/no-process-exit': 'error',
     'unicorn/number-literal-case': 'error',
+    'unicorn/prefer-add-event-listener': 'error',
     'unicorn/prefer-starts-ends-with': 'error',
     'unicorn/prefer-type-error': 'error',
     'unicorn/throw-new-error': 'error'
