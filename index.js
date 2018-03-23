ceb6c954eda66d54c9600dd89f6d28e20f6fe14f
Michael Novotny <manovotny@gmail.com>
2018-03-23 15:22:49 -0500

    Updates eslint-plugin-node.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = {
         ecmaFeatures: {
             jsx: true
         },
-        ecmaVersion: 2017,
+        ecmaVersion: 2018,
         sourceType: 'module'
     },
     plugins: [
