547638808a1f54b169ff23476b52d6010c03a93c
Michael Novotny <manovotny@gmail.com>
2019-02-25 22:55:17 -0600

    Updates eslint-plugin-node.

--- src/node.js
+++ src/node.js
@@ -6,6 +6,16 @@ module.exports = {
     'node/no-unpublished-bin': 'error',
     'node/no-unpublished-import': 'error',
     'node/no-unpublished-require': 'error',
+    'node/no-unsupported-features/es-builtins': 'error',
+    'node/no-unsupported-features/es-syntax': 'error',
+    'node/no-unsupported-features/node-builtins': 'error',
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'node/prefer-global/text-decoder': ['error', 'always'],
+    'node/prefer-global/text-encoder': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
     'node/process-exit-as-throw': 'error',
     'node/shebang': 'error'
 };
