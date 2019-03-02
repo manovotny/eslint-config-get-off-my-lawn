de4d75a475775b22b17c4f5943332d03107687c9
Michael Novotny <manovotny@gmail.com>
2019-03-02 12:07:36 -0600

    Updates eslint-plugin-jest.

--- src/jest.js
+++ src/jest.js
@@ -6,12 +6,24 @@ module.exports = {
             withinDescribe: 'test'
         }
     ],
+    'jest/no-alias-methods': 'error',
     'jest/no-disabled-tests': 'error',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
+    'jest/no-jest-import': 'error',
+    'jest/no-test-callback': 'error',
+    'jest/no-test-prefixes': 'error',
+    'jest/no-test-return-statement': 'error',
+    'jest/no-truthy-falsy': 'error',
+    'jest/prefer-called-with': 'error',
+    'jest/prefer-strict-equal': 'error',
     'jest/prefer-to-be-null': 'error',
+    'jest/prefer-to-contain': 'error',
+    'jest/prefer-todo': 'error',
+    'jest/prefer-spy-on': 'error',
     'jest/prefer-to-be-undefined': 'error',
     'jest/prefer-to-have-length': 'error',
+    'jest/require-tothrow-message': 'error',
     'jest/valid-describe': 'error',
     'jest/valid-expect': 'error'
 };
