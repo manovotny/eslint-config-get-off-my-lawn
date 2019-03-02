24888c0fe8c1ff765e70f37324639e854ee5f007
Michael Novotny <manovotny@gmail.com>
2019-03-02 12:19:04 -0600

    Updates eslint-plugin-jsx-a11y.

--- src/jsx-a11y.js
+++ src/jsx-a11y.js
@@ -9,19 +9,13 @@ module.exports = {
     'jsx-a11y/aria-role': 'error',
     'jsx-a11y/aria-unsupported-elements': 'error',
     'jsx-a11y/click-events-have-key-events': 'error',
+    'jsx-a11y/control-has-associated-label': 'error',
     'jsx-a11y/heading-has-content': 'error',
     'jsx-a11y/html-has-lang': 'error',
     'jsx-a11y/iframe-has-title': 'error',
     'jsx-a11y/img-redundant-alt': 'error',
     'jsx-a11y/interactive-supports-focus': 'error',
-    'jsx-a11y/label-has-for': [
-        'error',
-        {
-            required: {
-                every: ['nesting']
-            }
-        }
-    ],
+    'jsx-a11y/label-has-associated-control': 'error',
     'jsx-a11y/lang': 'error',
     'jsx-a11y/media-has-caption': 'error',
     'jsx-a11y/mouse-events-have-key-events': 'error',
