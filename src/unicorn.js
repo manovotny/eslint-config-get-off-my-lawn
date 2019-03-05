1806892957718ba4253a83cbc181737022570e76
Michael Novotny <manovotny@gmail.com>
2019-03-04 22:21:46 -0600

    Updates eslint-plugin-unicorn.

--- src/unicorn.js
+++ src/unicorn.js
@@ -1,21 +1,28 @@
 module.exports = {
-    'unicorn/catch-error-name': [
-        'error',
-        {
-            name: 'error'
-        }
-    ],
+    'unicorn/catch-error-name': 'error',
     'unicorn/custom-error-definition': 'error',
     'unicorn/error-message': 'error',
     'unicorn/escape-case': 'error',
+    'unicorn/filename-case': [
+        'error',
+        {
+            case: 'kebabCase'
+        }
+    ],
     'unicorn/new-for-builtins': 'error',
+    'unicorn/no-abusive-eslint-disable': 'error',
     'unicorn/no-array-instanceof': 'error',
+    'unicorn/no-console-spaces': 'error',
     'unicorn/no-hex-escape': 'error',
     'unicorn/no-new-buffer': 'error',
     'unicorn/no-process-exit': 'error',
+    'unicorn/no-unused-properties': 'error',
     'unicorn/number-literal-case': 'error',
     'unicorn/prefer-add-event-listener': 'error',
+    'unicorn/prefer-node-append': 'error',
+    'unicorn/prefer-query-selector': 'error',
     'unicorn/prefer-starts-ends-with': 'error',
     'unicorn/prefer-type-error': 'error',
+    'unicorn/regex-shorthand': 'error',
     'unicorn/throw-new-error': 'error'
 };
