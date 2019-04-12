c8ced457ffd315beddc790c4c75fc239f7a03e58
Michael Novotny <manovotny@gmail.com>
2019-04-12 16:11:02 -0500

    Removes no-process-exit duplication.
    Fixes #98

--- src/unicorn.js
+++ src/unicorn.js
@@ -15,7 +15,6 @@ module.exports = {
     'unicorn/no-console-spaces': 'error',
     'unicorn/no-hex-escape': 'error',
     'unicorn/no-new-buffer': 'error',
-    'unicorn/no-process-exit': 'error',
     'unicorn/no-unused-properties': 'error',
     'unicorn/number-literal-case': 'error',
     'unicorn/prefer-add-event-listener': 'error',
