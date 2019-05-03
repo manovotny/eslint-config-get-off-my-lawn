60824d0e1364b592516c29f91157a31ee89f19ad
Michael Novotny <manovotny@gmail.com>
2019-05-02 22:00:01 -0500

    Dynamically builds config based on dependencies.

--- index.js
+++ index.js
@@ -15,68 +15,83 @@ const reactNative = require('./src/react-native');
 const security = require('./src/security');
 const unicorn = require('./src/unicorn');
 
-const getReactVesion = () => {
-    const pkg = readPkgUp.sync() || {};
+const pkg = readPkgUp.sync() || {};
 
-    if (dotProp.get(pkg, 'pkg.dependencies.react') || dotProp.get(pkg, 'pkg.devDependencies.react')) {
-        return 'detect';
-    }
+const detectUsage = (dependency) =>
+    dotProp.get(pkg, `pkg.dependencies.${dependency}`) || dotProp.get(pkg, `pkg.devDependencies.${dependency}`);
 
-    return '16';
-};
-
-module.exports = {
+const config = {
     env: {
         browser: true,
-        jasmine: true,
-        jest: true,
-        node: true,
-        'react-native/react-native': true
+        node: true
     },
     parser: 'babel-eslint',
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
         ecmaVersion: 2019,
         sourceType: 'module'
     },
-    plugins: [
-        'eslint-comments',
-        'get-off-my-lawn',
-        'import',
-        'jest',
-        'json',
-        'jsx-a11y',
-        'node',
-        'objects',
-        'react',
-        'react-hooks',
-        'react-native',
-        'security',
-        'unicorn'
-    ],
+    plugins: ['eslint-comments', 'get-off-my-lawn', 'import', 'json', 'node', 'objects', 'security', 'unicorn'],
     rules: {
         ...eslint,
         ...eslintComments,
         ...getOffMyLawn,
         ...imprt,
-        ...jestRules,
-        ...jsxA11y,
         ...node,
         ...objects,
-        ...react,
-        ...reactHooks,
-        ...reactNative,
         ...security,
         ...unicorn
-    },
-    settings: {
-        node: {
-            allowModules: ['electron']
-        },
-        react: {
-            version: getReactVesion()
-        }
     }
 };
+
+if (detectUsage('electron')) {
+    dotProp.set(config, 'settings.node.allowModules', ['electron']);
+}
+
+if (detectUsage('jest')) {
+    dotProp.set(config, 'env.jasmine', true);
+    dotProp.set(config, 'env.jest', true);
+    config.plugins.push('jest');
+    config.rules = {
+        ...config.rules,
+        ...jestRules
+    };
+}
+
+if (detectUsage('react')) {
+    dotProp.set(config, 'parserOptions.ecmaFeatures.jsx', true);
+    dotProp.set(config, 'settings.react.version', 'detect');
+    config.plugins.push('react');
+    config.plugins.push('react-hooks');
+    config.rules = {
+        ...config.rules,
+        ...react,
+        ...reactHooks
+    };
+
+    if (detectUsage('react-native')) {
+        dotProp.set(config, 'env.react-native/react-native', true);
+        config.plugins.push('react-native');
+        config.rules = {
+            ...config.rules,
+            ...reactNative
+        };
+    } else {
+        config.plugins.push('jsx-a11y');
+        config.rules = {
+            ...config.rules,
+            ...jsxA11y
+        };
+    }
+}
+
+if (detectUsage('prettier')) {
+    config.rules = {
+        ...config.rules,
+        ...require('eslint-config-prettier'),
+        ...require('eslint-config-prettier/babel'),
+        ...require('eslint-config-prettier/react'),
+        ...require('eslint-config-prettier/unicorn')
+    };
+}
+
+module.exports = config;
