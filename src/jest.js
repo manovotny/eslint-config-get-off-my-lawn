2e209d4846d168fe4cfc34e4aa0f00fc16eee5b8
Michael Novotny <manovotny@gmail.com>
2019-08-14 16:00:05 -0500

    Adds `jest/no-expect-resolves` rule.

--- src/jest.js
+++ src/jest.js
@@ -11,6 +11,7 @@ module.exports = {
     'jest/no-disabled-tests': 'error',
     'jest/no-duplicate-hooks': 'error',
     'jest/no-empty-title': 'error',
+    'jest/no-expect-resolves': 'error',
     'jest/no-export': 'error',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
