b7fa4c25ad55859769ae96d655e9577f2ade0948
Michael Novotny <manovotny@gmail.com>
2021-02-28 16:36:50 -0600

    Fixes typo in get-off-my-lawn/prefer-length-truthiness rule name.

--- src/get-off-my-lawn.js
+++ src/get-off-my-lawn.js
@@ -1,4 +1,4 @@
 module.exports = {
     'get-off-my-lawn/prefer-arrow-functions': 'error',
-    'get-off-my-lawn/prprefer-length-truthiness': 'error',
+    'get-off-my-lawn/prefer-length-truthiness': 'error',
 };
