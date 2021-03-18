ad1ec25645e9130c56fa54a8d666d9e896d95751
Michael Novotny <manovotny@gmail.com>
2021-03-17 23:16:05 -0500

    Updates unicorn/string-content rule looking for https to ignore localhost.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    coverageDirectory: '.coverage',
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
+    testEnvironment: 'node',
+};
