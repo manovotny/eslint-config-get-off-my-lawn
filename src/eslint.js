f4d633e0ea71fff35a041a9c76d70d023d00fe8d
Michael Novotny <manovotny@gmail.com>
2021-12-09 16:37:21 -0600

    Updates to ESlint 8. (#175)

--- src/eslint.js
+++ src/eslint.js
@@ -235,6 +235,7 @@ module.exports = {
     ],
     'no-unused-expressions': 'error',
     'no-unused-labels': 'error',
+    'no-unused-private-class-members': 'error',
     'no-unused-vars': [
         'error',
         {
