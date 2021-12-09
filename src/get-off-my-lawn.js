9f10d5d94eab46ec5769825d99df24e78e93f339
Michael Novotny <manovotny@gmail.com>
2021-12-09 16:48:47 -0600

    Updates eslint-plugin-get-off-my-lawn.

--- src/get-off-my-lawn.js
+++ src/get-off-my-lawn.js
@@ -1,5 +1,4 @@
 module.exports = {
-    'get-off-my-lawn/no-empty-files': 'error',
     'get-off-my-lawn/prefer-arrow-functions': 'error',
     'get-off-my-lawn/prefer-length-truthiness': 'error',
 };
