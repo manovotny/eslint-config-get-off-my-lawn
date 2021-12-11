a3defa5fe0bca8fa5e1d3ad2ff179a256571ecd6
Michael Novotny <manovotny@gmail.com>
2021-12-11 16:16:08 -0600

    Changes eslint-plugin-jest-formatting to use individual rules instead of the meta catch all rule for better error messages.

--- src/jest-formatting.js
+++ src/jest-formatting.js
@@ -1,3 +1,9 @@
 module.exports = {
-    'jest-formatting/padding-around-all': 'error',
+    'jest-formatting/padding-around-after-all-blocks': 'error',
+    'jest-formatting/padding-around-after-each-blocks': 'error',
+    'jest-formatting/padding-around-before-all-blocks': 'error',
+    'jest-formatting/padding-around-before-each-blocks': 'error',
+    'jest-formatting/padding-around-describe-blocks': 'error',
+    'jest-formatting/padding-around-expect-groups': 'error',
+    'jest-formatting/padding-around-test-blocks': 'error',
 };
