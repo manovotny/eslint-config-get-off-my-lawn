3d68830c4a34d59da5d64dbf9171e2a05ccb6282
Michael Novotny <manovotny@gmail.com>
2022-01-19 14:40:09 -0600

    Updates dependencies.

--- src/jest.js
+++ src/jest.js
@@ -46,6 +46,8 @@ const config = {
         'jest/no-test-prefixes': 'error',
         'jest/no-test-return-statement': 'error',
         'jest/prefer-called-with': 'error',
+        'jest/prefer-comparison-matcher': 'error',
+        'jest/prefer-equality-matcher': 'error',
         'jest/prefer-expect-resolves': 'error',
         'jest/prefer-hooks-on-top': 'error',
         'jest/prefer-spy-on': 'error',
