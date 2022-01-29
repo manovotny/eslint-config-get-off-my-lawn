de94c7a48dacc4ccad52958ddabccd3f517b2d78
Michael Novotny <manovotny@gmail.com>
2022-01-29 13:35:02 -0600

    Removes eslint-comments/no-unlimited-disable as it is a duplicate of unicorn/no-abusive-eslint-disable.

--- src/comments.js
+++ src/comments.js
@@ -3,7 +3,7 @@ module.exports = {
     rules: {
         'eslint-comments/disable-enable-pair': 'error',
         'eslint-comments/no-duplicate-disable': 'error',
-        'eslint-comments/no-unlimited-disable': 'error',
+        // eslint-comments/no-unlimited-disable is a duplicate of unicorn/no-abusive-eslint-disable
         'eslint-comments/no-unused-disable': 'error',
         'eslint-comments/no-unused-enable': 'error',
     },
