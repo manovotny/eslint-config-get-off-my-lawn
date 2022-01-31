a60796440606348ba740bb336ea95254a8a36041
Michael Novotny <manovotny@gmail.com>
2022-01-31 17:30:33 -0600

    Refactors test strucrture.

--- src/import.test.js
+++ src/import.test.js
@@ -1,33 +1,31 @@
-describe('import', () => {
-    describe('import/extensions', () => {
-        beforeEach(() => {
-            jest.resetModules();
-        });
+describe('import/extensions', () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
 
-        [
-            {
-                expectedConfiguration: 'never',
-                expectedExtensions: {mjs: 'ignorePackages'},
-                mockType: undefined,
-            },
-            {
-                expectedConfiguration: 'ignorePackages',
-                expectedExtensions: {cjs: 'never'},
-                mockType: 'module',
-            },
-        ].forEach(({expectedConfiguration, expectedExtensions, mockType}) => {
-            test(`packageJson.type: ${mockType}`, () => {
-                jest.doMock('./utils/files/contents', () => ({
-                    packageJson: {type: mockType},
-                }));
+    [
+        {
+            expectedConfiguration: 'never',
+            expectedExtensions: {mjs: 'ignorePackages'},
+            mockType: undefined,
+        },
+        {
+            expectedConfiguration: 'ignorePackages',
+            expectedExtensions: {cjs: 'never'},
+            mockType: 'module',
+        },
+    ].forEach(({expectedConfiguration, expectedExtensions, mockType}) => {
+        test(`packageJson.type: ${mockType}`, () => {
+            jest.doMock('./utils/files/contents', () => ({
+                packageJson: {type: mockType},
+            }));
 
-                const {rules} = require('./import');
+            const {rules} = require('./import');
 
-                const [, configuraton, extensions] = rules['import/extensions'];
+            const [, configuraton, extensions] = rules['import/extensions'];
 
-                expect(configuraton).toBe(expectedConfiguration);
-                expect(extensions).toStrictEqual(expectedExtensions);
-            });
+            expect(configuraton).toBe(expectedConfiguration);
+            expect(extensions).toStrictEqual(expectedExtensions);
         });
     });
 });
