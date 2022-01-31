b7128f69f7562dc81e6cad77ca5e8088905d51be
Michael Novotny <manovotny@gmail.com>
2022-01-31 11:03:32 -0600

    Changes node globals based on node documentation.

--- src/node.js
+++ src/node.js
@@ -17,13 +17,17 @@ module.exports = {
         'node/no-unpublished-require': 'error',
         'node/no-unsupported-features/es-builtins': 'error',
         'node/no-unsupported-features/node-builtins': 'error',
+        // While the `Buffer` class is available within the global scope, it is still recommended to explicitly reference it via an import or require statement.
+        // https://nodejs.org/api/buffer.html#buffer
         'node/prefer-global/buffer': ['error', 'never'],
-        'node/prefer-global/console': ['error', 'never'],
+        'node/prefer-global/console': ['error', 'always'],
+        // The `process` object provides information about, and control over, the current Node.js process. While it is available as a global, it is recommended to explicitly access it via require or import.
+        // https://nodejs.org/api/process.html#process
         'node/prefer-global/process': ['error', 'never'],
-        'node/prefer-global/text-decoder': ['error', 'never'],
-        'node/prefer-global/text-encoder': ['error', 'never'],
-        'node/prefer-global/url': ['error', 'never'],
-        'node/prefer-global/url-search-params': ['error', 'never'],
+        'node/prefer-global/text-decoder': ['error', 'always'],
+        'node/prefer-global/text-encoder': ['error', 'always'],
+        'node/prefer-global/url': ['error', 'always'],
+        'node/prefer-global/url-search-params': ['error', 'always'],
         'node/prefer-promises/dns': 'error',
         'node/prefer-promises/fs': 'error',
         'node/process-exit-as-throw': 'error',
