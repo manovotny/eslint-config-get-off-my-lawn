86eb4b6891a6924f985644f0201cbfa161f3b6e3
Michael Novotny <manovotny@gmail.com>
2022-03-20 21:09:05 -0500

    Fixes false positives with `import/extensions` in ESM.

--- src/import.test.js
+++ src/import.test.js
@@ -1,9 +1,41 @@
+describe('import/extensions', () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
+
+    test('cjs', () => {
+        jest.doMock('./utils/files/contents', () => ({
+            packageJson: {},
+        }));
+
+        const {rules} = require('./import');
+
+        const [, option, config] = rules['import/extensions'];
+
+        expect(option).toBe('never');
+        expect(config.mjs).toBe('ignorePackages');
+    });
+
+    test('esm', () => {
+        jest.doMock('./utils/files/contents', () => ({
+            packageJson: {type: 'module'},
+        }));
+
+        const {rules} = require('./import');
+
+        const [, option, config] = rules['import/extensions'];
+
+        expect(option).toBe('ignorePackages');
+        expect(config.cjs).toBe('never');
+    });
+});
+
 describe('import/no-useless-path-segments', () => {
     beforeEach(() => {
         jest.resetModules();
     });
 
-    test('packageJson.type = "module"', () => {
+    test('esm', () => {
         jest.doMock('./utils/files/contents', () => ({
             packageJson: {type: 'module'},
         }));
