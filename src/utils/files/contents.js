427268268a1e632ff59c197cac404f12b0bbc888
Michael Novotny <manovotny@gmail.com>
2022-07-26 13:31:40 -0500

    Fixes issues with trying to read an ESM Jest config.

--- src/utils/files/contents.js
+++ src/utils/files/contents.js
@@ -2,9 +2,22 @@ const {readJsonSync} = require('fs-extra');
 
 const locate = require('./locate');
 
-const packageJson = readJsonSync(locate('package.json'), {throws: false}) || {};
 const jestConfigLocation = locate(['jest.config.cjs', 'jest.config.js', 'jest.config.ts']);
-const jestConfig = jestConfigLocation ? require(jestConfigLocation) : {};
+
+const getJestConfig = () => {
+    if (jestConfigLocation) {
+        try {
+            require(jestConfigLocation);
+        } catch {
+            return {};
+        }
+    }
+
+    return {};
+};
+
+const packageJson = readJsonSync(locate('package.json'), {throws: false}) || {};
+const jestConfig = getJestConfig();
 const tsConfig = readJsonSync(locate('tsconfig.json'), {throws: false}) || {};
 
 module.exports = {
